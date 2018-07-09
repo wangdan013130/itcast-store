78fe10db8186c4e3d3a5ac19d98afa5cfc6d4ecd
wangdan013130 <2351290437@qq.com>
2018-07-10 00:31:28 +0800

    登录首页展示

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Login from '@/views/Login.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      name: 'login',
+      path: '/login',
+      component: Login
+    },
     {
       path: '/',
       name: 'HelloWorld',
---
+++ src/views/Login.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="login-wrap">
+  <el-form
+    class="login-from"
+    label-position="top"
+    label-width="80px"
+    :model="formData">
+    <h2>用户登录</h2>
+    <el-form-item label="用户名">
+      <el-input type="text" v-model="formData.username"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input type="password" v-model="formData.password"></el-input>
+    </el-form-item>
+    <el-button type="primary"  class="login-btn" @click="handelLogin">登录</el-button>
+  </el-form>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      formData: {
+        username: '',
+        passeord: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.login-wrap {
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-wrap .login-from {
+  background-color: #fff;
+  width: 400px;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+.login-wrap .login-from .login-btn {
+  width: 100%;
+}
+</style>
